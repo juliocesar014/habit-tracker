7a6bebabb7b9481c172a978e519b38d01fc0289f
juliocesar014 <juliocesar014@live.com>
2022-12-30 20:21:02 -0300

    feat/ initial config in flask app

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
+from db import db
 
 app = Flask(__name__)
+habits = ['Test Habit','Tets Habit 2']
+
 
 @app.route("/")
-def home():
-    return render_template("index.html", title = "Habit Tracker - Home")
+def index():
+    return render_template("index.html", habits=habits ,title = "Habit Tracker - Home")
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add_habit():
-    return render_template("add_habit.html", title = "Habit Tracker - Add new")
+    if request.method == "POST":
+        habit = request.form.get("habit")
+        habits.append(habit)
+        return redirect(url_for('index'))
+    else:
+        error = 'Invalid Habit Tracker'
+        return render_template("add_habit.html", title = "Habit Tracker - Add new", error=error)
+    
 
 
 if __name__ == '__main__':
