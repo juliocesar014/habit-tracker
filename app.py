9620a6841c8456f4d939149284cc8368260c1d9d
juliocesar014 <juliocesar014@live.com>
2022-12-30 10:59:45 -0300

    initial configs for app-flask

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html", title = "Habit Tracker - Home")
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_habit():
+    return render_template("add_habit.html", title = "Habit Tracker - Add new")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    
